6233081d5b95e7b98e3ed71f062ceaa0bcc6d972
Dharunkumar <dharunkumarsengottuvelu@gmail.com>
2026-01-29 20:51:42 +0530

    Add files via upload

---
+++ Nested Loop/P7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,j,k;
+    for (i=1;i<=5;i++){
+        for (k=1;k<=5-i;k++){
+            printf(" ");
+        }
+        for (j=1;j<=2*i-1;j++){
+            printf("%c",j=64);
+        }
+        printf("\n");
+    }
+    
+}
